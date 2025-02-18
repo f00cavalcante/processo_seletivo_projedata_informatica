1140d4bc639813bb8f3e1b192ddad3d7300dc8a9
Fellipe Cavalcante <fc.g.hub@f00cavalcante.com>
2025-02-18 09:28:09 -0300

    Idade dos Funcionarios
    Nesta classe, foi trabalhado o processo para retornar a data de nascimento e idade dos funcionários, com base na lista original, organizada por nome, isto é, a chave tornou-se o nome dos funcionários, e os valores seus dados. Com isto, está sendo criado retornos para:
    - Um novo dicionário como chave sendo o nome de dos funcionários, e os valores a data de nascimento e idade;
    - Uma String com o nome do funcionário mais velho, portanto, a respectiva chave, para reconher o valor ao exibir no System.out.println.

---
+++ Funcionarios/IdadeDosFuncionarios.java
@@ -0,0 +1,51 @@
+package Funcionarios;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class IdadeDosFuncionarios {
+
+     public Map<String, Map<String, String>> DicionarioDataNascimentoFuncionario = new HashMap<>();
+
+     public IdadeDosFuncionarios(List<Map<String, Map<String, String>>> ListaDosFuncionarios) {
+
+          for (Map<String, Map<String, String>> funcionario : ListaDosFuncionarios) {
+
+               for (Map<String, String> dados : funcionario.values()) {
+
+                    DateTimeFormatter formatarDataDeNascimento = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    LocalDate dataNascimentoFuncionario = LocalDate.parse(dados.get("Data Nascimento"), formatarDataDeNascimento);
+                    long idadeDoFuncionario = dataNascimentoFuncionario.until(LocalDate.now(), ChronoUnit.YEARS);
+
+                    Map<String, String> dadosDoNascimentoDoFuncionarioTemporario = new HashMap<>();
+                    dadosDoNascimentoDoFuncionarioTemporario.put("Data Nascimento", dataNascimentoFuncionario.toString());
+                    dadosDoNascimentoDoFuncionarioTemporario.put("Idade", Long.toString(idadeDoFuncionario));
+
+                    DicionarioDataNascimentoFuncionario.put(funcionario.keySet().toString(), dadosDoNascimentoDoFuncionarioTemporario);
+               }
+          }
+     }
+
+     public String FuncionarioMaisVelho() {
+
+          int idadeDoFuncionarioMaisVelho = 0;
+          String nomeDoFuncionarioMaisVelho = new String();
+
+          for (String funcionario : DicionarioDataNascimentoFuncionario.keySet()) {
+
+               if (Integer.valueOf(DicionarioDataNascimentoFuncionario.get(funcionario).get("Idade")) > idadeDoFuncionarioMaisVelho) {
+
+                    idadeDoFuncionarioMaisVelho = Integer.valueOf(DicionarioDataNascimentoFuncionario.get(funcionario).get("Idade"));
+                    nomeDoFuncionarioMaisVelho = funcionario;
+               }
+          }
+
+
+          return nomeDoFuncionarioMaisVelho;
+     }
+}
